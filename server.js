de0677f8c1ce01ec5e23b26c05c7164c73b43e31
Joshua Ogbonna <devjaykes@gmail.com>
2021-06-22 09:53:17 +0100

    Test routes

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+// Import packages
+const morgan = require('morgan')
+const cors = require('cors')
+
+// Use packages
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.get('/api/users', (req, res) => {
+    res.json({
+        name: "Joshua Ogbonna",
+        occupation: "Software Developer",
+        age: 24
+    })
+})
+
+const PORT = 30000
+
+app.listen(PORT, () => {
+    console.log(`Server started on Port ${PORT}`)
+})
