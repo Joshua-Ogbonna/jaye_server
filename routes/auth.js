2d85883ab43f267e8b4d7cb35eab4caeb6c8d7eb
Joshua Ogbonna <devjaykes@gmail.com>
2021-06-23 16:30:30 +0100

    Login route complete

--- routes/auth.js
+++ routes/auth.js
@@ -39,4 +39,41 @@ router.post('/signup', (req, res) => {
   }).catch(err => console.log(err))
 })
 
+// Login route
+router.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    // Check if there's a user
+    if (!user) {
+      return res.status(404).json({
+        message: 'user not found',
+        success: false
+      })
+    } else {
+      bcrypt.compare(req.body.password, user.password).then(isMatch => {
+        if (isMatch) {
+          const payload = {
+            _id: user._id,
+            name: user.name,
+            email: user.email
+          }
+          jwt.sign(payload, process.env.SECRET, { expiresIn: '24h' }, (err, token) => {
+            if (err) throw err
+            return res.status(201).json({
+              data: payload,
+              token: `Toke ${token}`
+            })
+          })
+        } else {
+          return res.status(404).json({
+            message: 'user not found',
+            success: false
+          })
+        }
+      })
+    }
+  }).catch(err => {
+    return res.json(err)
+  })
+})
+
 module.exports = router
