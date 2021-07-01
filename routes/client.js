6f3a70fba1e097d0a0141c51044725fb9f91e5a5
Joshua Ogbonna <devjaykes@gmail.com>
2021-07-01 13:04:48 +0100

    This is some bug

--- routes/client.js
+++ routes/client.js
@@ -64,7 +64,7 @@ router.get('/client/:id', async (req, res) => {
 router.put('/client/:id', async (req, res) => {
   await Client.findById({ _id: req.params.id })
     .then((client) => {
-      client.notes.push(req.body.body)
+      client.notes.push(req.body)
       client.save()
       res.status(201).json({
         success: true,
